72ff61be6503d13523dcefbaa1b526750eee523a
Mike Meehan <mjmeehan@gmail.com>
2013-03-16 10:39:54 -0400

    Cleaned up flow

--- hero.pl
+++ hero.pl
@@ -1,18 +1,58 @@
 #!/usr/bin/perl
-#
 use Games::Dice qw(roll_array roll);
 use List::Util qw(reduce sum);
 use POSIX qw(floor);
 use Term::ANSIColor;
 use Getopt::Std;
-
+use Switch;
 #use warnings;
 use strict;
+
+sub colorize;
+
 our $opt_k;
 my $dice = $ARGV[0];
 my ($stun, $body);
 getopt('k:');
 my @roll;
+
+my $mode;
+if($opt_k) {
+	$mode = "Killing attack";
+} elsif (scalar(@ARGV) == 1) {
+	$mode = "Normal attack";
+} else {
+	$mode = "Skill Test";
+}
+
+print "$mode\n";
+my (@roll, $stun, $body);
+switch ($mode) {
+case "Killing attack" {
+	@roll = roll_array($opt_k . "d6");
+	colorize(@roll);
+	$body = sum @roll;
+	my $kill = roll('1d6') - 1;
+	$kill = $kill == 0 ? 1 : $kill;
+	$stun = $body * $kill;
+	print "$body * $kill = $stun\n";
+} case "Normal attack" {
+	@roll = roll_array($dice . "d6");
+	colorize(@roll);
+	$stun =  sum @roll;
+	$body = reduce { $a + floor($b/2) } 0, @roll;
+} else {
+	@roll = roll_array("3d6");
+	print join(" ", @roll) . "\n";
+	print sum(@roll) . "\n";
+	exit(1);
+}
+}
+
+print "Stun: $stun\n";
+print "Body: $body\n";
+
+########
 
 sub colorize {
 	(@roll) = @_;
@@ -30,29 +70,3 @@ sub colorize {
 	}
 	print "\n";
 }
-
-if($opt_k) {
-	print "Killing attack\n";
-	@roll = roll_array($opt_k . "d6");
-	colorize(@roll);
-	$body = sum @roll;
-	my $kill = roll('1d6') - 1;
-	$kill = $kill == 0 ? 1 : $kill;
-	$stun = $body * $kill;
-	print "$body * $kill = $stun\n";
-} elsif (scalar(@ARGV) == 1) {
-	print "Normal attack\n";
-	@roll = roll_array($dice . "d6");
-	colorize(@roll);
-	$stun =  sum @roll;
-	$body = reduce { $a + floor($b/2) } 0, @roll;
-} else {
-	print "Skill test\n";
-	@roll = roll_array("3d6");
-	print join(" ", @roll) . "\n";
-	print sum(@roll) . "\n";
-	exit(1);
-}
-
-print "Stun: $stun\n";
-print "Body: $body\n";
